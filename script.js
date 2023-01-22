7457c4ee2f95e8aa629a322cf540dc7c2ae07804
hz-zh <115835509+hz-zh@users.noreply.github.com>
2023-01-22 16:50:55 -0500

    Add delay to each square on creation & removal. See desc.
    Additionally: make the container a fixed size, remove gapping between
    squares, make squares dynamically sized based on user input.

--- script.js
+++ script.js
@@ -1,61 +1,76 @@
 const container = document.querySelector('.container');
 const input = document.querySelector('.input');
-const createBoard = document.querySelector('.createBoard');
-const resetBoard = document.querySelector('.resetBoard');
+const toggleBoard = document.querySelector('.toggleBoard');
 
-input.value = 'Enter a single board dimension between 3 and 15.';
+input.value = 'Enter a single board dimension between 3 and 18.';
 
 let createBoardClicked = false;
+let num;
 
-createBoard.onclick = function() { 
-   if (createBoardClicked == true) return;
+toggleBoard.onclick = function() { 
+   if (createBoardClicked == true) resetGameBoard();
    else getNum();
  };
 
 input.onclick = function() {
+   toggleBoard.style.display = 'flex';
    input.value = '';
    input.setAttribute('style', 'font-size: 30px; color: rgb(0, 0, 0);');
 };
 
 function resetInput() {
    input.setAttribute('style', 'color:rgba(0, 0, 0, 0.513); font-size: 18px;');
-   input.value = 'Enter a single dimension between 3 and 15.';
+   input.value = 'Enter a single board dimension between 3 and 18.';
 };
 
 function makeSquares (num) {
-   for (i = 0; i < (num * num); i++) {
+   let i = 0;
+   let intervalID;
+   
+   intervalID = setInterval(function(){
+      createBoardClicked = true;
       const square = document.createElement('div');
 
-      square.setAttribute('style', 'background: blue; width: 2em; height: 2em');
-      square.setAttribute('id', 'boardDiv');
-
-      container.setAttribute('style', `max-width: ${(num * 2) + (num * .5) - 0.5}em;`);
+      square.setAttribute('style', `background: rgb(54, 14, 14); width: ${575/num}px; height: ${575/num}px; border: solid 0px;`);
 
       container.appendChild(square);
-   }
-   resetBoard.onclick = function() { resetGameBoard(num) };
+
+      container.setAttribute('style', `max-width: 575px`);
+
+      i++;
+      if (i == (num * num)) clearInterval(intervalID);
+   });
+   toggleBoard.textContent = 'reset board';
+
+   let colors = ['red', 'blue', 'green', 'teal', 'rosybrown', 'tan', 'plum', 'saddlebrown'];
+   container.addEventListener('mouseover', function(event) {
+      event.target.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    }, false); 
 };
 
 function getNum () {
-   let num = input.value;
-   if (num > 15 || num < 3) { 
-      alert('Please enter a value between 3 and 15.');
+   num = input.value;
+   if (num > 60 || num < 3 || 
+      num == 'Enter a single board dimension between 3 and 18.') 
+      { 
+      alert('Please enter a value between 3 and 18.');
       resetInput();
       return;
-   }
+      }
    makeSquares(num);
-   createBoardClicked = true;
 };
 
-function resetGameBoard(num) {
+function resetGameBoard() {
+   toggleBoard.style.display = 'none';
    let element = document.querySelector(".container");
-   for (i = 0; i < (num * num); i++) {
-         remove();
-    };
+   let intervalID;
 
-    function remove() {
-      setInterval(function(){
-         element.removeChild(element.firstChild)}, 1000);
-    }
+         intervalID = setInterval(function(){
+            element.removeChild(element.firstChild);
+            if (!element.firstChild) clearInterval(intervalID);
+         }, (.5/num));
+
    createBoardClicked = false;
+   resetInput();
+   toggleBoard.textContent = 'create board';
 }
