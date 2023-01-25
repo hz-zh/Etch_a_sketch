3b406e8ece83e4151b7a2aea0c87019ee603d26a
hz-zh <115835509+hz-zh@users.noreply.github.com>
2023-01-25 18:14:57 -0500

    Change 'mystery' to standalone function, see desc.
    'mystery' now runs when user clicks the associated button.
    setInterval is now used to call colorPixel, which colors
    each child of 'container' using a forEach hack on the Nodelist,
    via colorChanger (which brushTool also uses). The anonymous
    function inside mystery's onclick event calls setInterval,
    which in turn calls colorPixel with a counter, which is then
    passed to colorChanger as a modifier on the three brushCounters,
    increasing the rate of each oscillator linearly. This produces
    an evolving trippy phasing effect on the board. Other functions
    may also be applied in the call to colorPixel.

--- script.js
+++ script.js
@@ -3,18 +3,13 @@ const input = document.querySelector('.input');
 const toggleBoard = document.querySelector('.toggleBoard');
 const random = document.querySelector('.random');
 const content = document.querySelector('.content');
-const colorBoard = document.querySelector('.colorBoard');
+const mystery = document.querySelector('.mystery');
 
 input.value = 'Enter a single board dimension between 3 and 60.';
 
 let createBoardClicked = false;
-let mouseOver = false;
+let brushActive = false;
 let num;
-
-toggleBoard.onclick = function() { 
-   if (createBoardClicked == true) resetGameBoard();
-   else getNum();
- };
 
 input.onclick = function() {
    toggleBoard.style.display = 'flex';
@@ -22,12 +17,28 @@ input.onclick = function() {
    input.setAttribute('style', 'font-size: 30px; color: rgb(0, 0, 0);');
 };
 
-colorBoard.onclick = function() {
-   if (!mouseOver){
-      colorBoard.style.backgroundColor = 'black';
-   }
-   else colorBoard.style.backgroundColor = 'rgb(31, 113, 2)';
-   mouseOver = !mouseOver;
+toggleBoard.onclick = function() { 
+   if (createBoardClicked == true) resetGameBoard();
+   else getNum();
+ };
+
+ random.onclick = function() {
+   brushCounter1 = randomAngle();
+   brushCounter2 = randomAngle();
+   brushCounter3 = randomAngle();
+};
+
+mystery.onclick = function() {
+   brushActive = true;
+
+   let i = 0;
+   let intervalID;
+   intervalID = setInterval(function(){
+      brushActive = true;
+      colorPixels(i);
+      i++;
+      if (i > 350) clearInterval(intervalID);
+   }, 110);
 }
 
 function resetInput() {
@@ -48,8 +59,6 @@ function getNum () {
    makeSquares(num);
 };
 
-let backgroundCounter = 0;
-
 function makeSquares (num) {
    let i = 0;
    let intervalID;
@@ -63,44 +72,53 @@ function makeSquares (num) {
 
       container.appendChild(square);
       container.setAttribute('style', `max-width: 600px;`);
-      if (mouseOver) {
-         colorChanger(square, 1, 5, 2);
-         colorBoard.textContent = 'YAY (unselect to brush)';
-      }
-
+      
       i++;
       if (i == (num * num)) clearInterval(intervalID);
-
    });
 
    toggleBoard.textContent = 'reset board';
    brushTool();
 };
 
-let brushCounter = 0;
+let brushCounter1 = 0;
 let brushCounter2 = 90;
 let brushCounter3 = 180;
 
 function brushTool() {
-   random.onclick = function() {
-      brushCounter = randomAngle();
-      brushCounter2 = randomAngle();
-      brushCounter3 = randomAngle();
-   };
+   let i = 0;
    container.addEventListener('mouseover', 
-   function(e) { colorChanger(e, 1, 2, 3)}, false); 
-}
+   function(e) { 
+      colorChanger(e, 1, 4, 9)
+      i++;
+   }, false);
+      
+};
 
 function randomAngle() {
    return Number(Math.floor(Math.random() * 180));
-}
-
-function colorPicker(color) {
-   let newColor = Number(255 - color);
-   return newColor;
 };
 
-function oscillate0(input) {
+ let startingColor = [0, 0, 0]; 
+
+ function colorChanger(e, increm1, increm2, increm3) {
+   startingColor[0] = oscillate0(brushCounter1);
+   startingColor[1] = oscillate1(brushCounter2);
+   startingColor[2] = oscillate2(brushCounter3);
+
+   brushCounter1 += increm1;
+   brushCounter2 += increm2;
+   brushCounter3 += increm3;
+   //console.log(startingColor[0], startingColor[1], startingColor[2]); 
+   if (brushActive) { e.style.backgroundColor = `rgb(${startingColor[0]} ${startingColor[1]} 
+   ${startingColor[2]})`;
+   }
+   else {
+   e.target.style.backgroundColor = `rgb(${startingColor[0]} ${startingColor[1]} ${startingColor[2]})`;
+   }
+ }; 
+
+ function oscillate0(input) {
    let max = 255;
    let min = 0;
    let range = max - min;
@@ -110,6 +128,7 @@ function oscillate0(input) {
    let max = 255;
    let min = 0;
    let range = max - min;
+   console.log(0 + Math.abs(((input + range) % (range * 2)) - range));
    return 0 + Math.abs(((input + range) % (range * 2)) - range);
  };
  function oscillate2(input) {
@@ -119,35 +138,22 @@ function oscillate0(input) {
    return 0 + Math.abs(((input + range) % (range * 2)) - range);
  };
 
- let startingColor = [0, 0, 0]; 
-
- function colorChanger(e, increm1, increm2, increm3) {
-   startingColor[0] = oscillate0(brushCounter);
-   startingColor[1] = oscillate1(brushCounter2);
-   startingColor[2] = oscillate2(brushCounter3);
-
-   brushCounter += increm1;
-   brushCounter2 += increm2;
-   brushCounter3 += increm3;
-   console.log(startingColor[0], startingColor[1], startingColor[2]);
-   if (mouseOver) { e.style.backgroundColor = `rgb(${colorPicker(startingColor[0])} ${colorPicker(startingColor[1])} 
-   ${colorPicker(startingColor[2])})`;
-   }
-   else {
-   e.target.style.backgroundColor = `rgb(${startingColor[0]} ${startingColor[1]} ${startingColor[2]})`;
-   }
- }; 
-
+ function colorPixels(i) {
+   NodeList.prototype.forEach = Array.prototype.forEach;
+   let children = container.childNodes;
+   children.forEach(function(item){
+      colorChanger(item, 1+i, 2+i, 4+i);
+   }); 
+   brushActive = false;
+};
 
 function resetGameBoard() {
    toggleBoard.style.display = 'none';
    let element = document.querySelector(".container");
-   let intervalID;
 
-         intervalID = setInterval(function(){
+         while (element.firstChild) {
             element.removeChild(element.firstChild);
-            if (!element.firstChild) clearInterval(intervalID);
-         });
+         }
 
    createBoardClicked = false;
    resetInput();
